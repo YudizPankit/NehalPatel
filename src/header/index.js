7275ada372698ee7d73491b141ae572e2420510f
Pankit chapla <pankit@yudiz.com>
2024-07-03 20:11:57 +0530

    Update index.js

--- src/header/index.js
+++ src/header/index.js
@@ -20,7 +20,7 @@ const Headermain = () => {
       <header className="fixed-top site__header">
         <div className="d-flex align-items-center justify-content-between">
           <Link  className="navbar-brand nav_ac" to="/">
-            {<img src={LogoHeader} width={50} height={50} alt="Bhargav" />}
+            {<img src={LogoHeader} width={80} height={80} alt="Nehal" />}
           </Link>
           <div className="d-flex align-items-center">
           <Themetoggle />
@@ -54,7 +54,7 @@ const Headermain = () => {
           </div>
           <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
             <div className="d-flex">
-            <a href={socialprofils.linkedin}>Linkedin</a>
+            <a href={socialprofils.Instagram}>Instagram</a>
             <a href={socialprofils.twitter}>Twitter</a>
             <a href={socialprofils.facebook}>Facebook</a>
             </div>
